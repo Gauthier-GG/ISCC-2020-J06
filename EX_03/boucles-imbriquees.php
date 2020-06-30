f1f67eba6537d73a8b02a9ac5bc7a551535fbd49
Gauthier-GG <67266035+Gauthier-GG@users.noreply.github.com>
2020-06-30 18:00:04 +0200

    PHP
    je sue du cerveau

---
+++ EX_03/boucles-imbriquees.php
@@ -0,0 +1,16 @@
+<?php
+function clock(){
+    for($minute=0;$minute<=59; $minute=$minute + 1){
+        if ($minute>59){
+            $heure + 1;
+        }
+    }
+    for($heure=0;$heure<=23;){
+        if ($heure>23){
+        break;
+        }
+    }
+}
+//Rendu//
+clock();
+?>
